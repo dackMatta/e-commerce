3f9a054b83d8a791e05b2e872d542ab0c04dc6b8
Entrix Makambi <makambientryx@gmail.com>
2024-05-30 09:44:22 +0300

    initial commit

---
+++ cart/views.py
@@ -0,0 +1,65 @@
+from django.shortcuts import render,redirect,get_object_or_404
+from django.views.decorators.http import require_POST
+from shop.models import Product
+from .cart import Cart
+from .forms import CartAddProductForm
+from shop.recommender import Recommender
+
+
+# Create your views here.
+@require_POST
+def cart_add(request,product_id):
+    cart=Cart(request)
+    product=get_object_or_404(Product,id=product_id)
+    form=CartAddProductForm(request.POST)
+    if form.is_valid():
+        cd=form.cleaned_data
+        cart.add(product=product,
+                 quantity=cd['quantity'],
+                 override_quantity=cd['override'])
+    return redirect('cart:cart_detail')
+
+@require_POST
+def cart_remove(request, product_id):
+    cart = Cart(request)
+    product = get_object_or_404(Product, id=product_id)
+    cart.remove(product)
+    return redirect('cart:cart_detail')
+
+def cart_detaiiil(request):
+    cart=Cart(request)
+    for item in cart:
+        item['update_quantity_form']=CartAddProductForm(initial={'quantity':item['quantity'],'override':True})
+
+    r=Recommender
+    cart_products=[item['product'] for item in cart]
+    if(cart_products):
+        recommended_products=r.suggest_products_for(
+            cart_products,
+            max_results=4
+        )
+
+    else:
+        recommended_products=[]
+    return render(request,'cart/detail.html',{'cart':cart,
+                                              'recommended_products':recommended_products})
+
+
+def cart_detail(request):
+    cart = Cart(request)
+    for item in cart:
+        item['update_quantity_form'] = CartAddProductForm(initial={'quantity': item['quantity'], 'override': True})
+
+    
+    r = Recommender()  # Create an instance of the Recommender class
+
+    cart_products = [item['product'] for item in cart]
+    recommended_products = []  # Initialize recommended_products list
+
+    if cart_products:  # Check if cart is not empty
+        recommended_products = r.suggest_products_for(
+            cart_products,
+            max_results=4
+        )
+
+    return render(request, 'cart/detail.html', {'cart': cart, 'recommended_products': recommended_products})
